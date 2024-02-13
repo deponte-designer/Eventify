0d9a66fae2135ac2a7073297d0e6f26b65e26fc3
Ponte, Nadia <deponte.work@gmail.com>
2024-02-13 19:46:09 +0000

    fixed the navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -22,14 +22,9 @@ const CustomNavbar = ({ onSearchSubmit }) => {
   return (
     <Navbar className="bg-body-tertiary-opacity nav-opacity mb-3" expand="lg" data-bs-theme="dark" fixed="top">
       <Container>
+        
         <Navbar.Brand className="" href="#">Eventify</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink className="nav-link" to="/" end>Home</NavLink>
-            <NavLink className="nav-link" to="/artists" end>Artists</NavLink>
-            <NavLink className="nav-link" to="/contact" end>Contact</NavLink>
-            <Form onSubmit={handleSearchSubmit} className="d-flex">
+        <Form onSubmit={handleSearchSubmit} className="d-flex d-lg-none">
               <Form.Control
                 type="search"
                 placeholder="Search by Artist..."
@@ -41,9 +36,29 @@ const CustomNavbar = ({ onSearchSubmit }) => {
               />
               <Button variant="outline-success" className="btn-outline-purple" type="submit">Search</Button>
             </Form>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink className="nav-link" to="/" end>Home</NavLink>
+            <NavLink className="nav-link" to="/artists" end>Artists</NavLink>
+            <NavLink className="nav-link" to="/contact" end>Contact</NavLink>
+            
           </Nav>
         </Navbar.Collapse>
-      </Container>
+        <Form onSubmit={handleSearchSubmit} className="d-lg-flex d-sm-none">
+              <Form.Control
+                type="search"
+                placeholder="Search by Artist..."
+                className="me-2"
+                aria-label="Search"
+                value={searchQuery}
+                // onChange={handleSearchChange}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+              <Button variant="outline-success" className="btn-outline-purple" type="submit">Search</Button>
+            </Form>
+
+            </Container>
     </Navbar>
 
   );
