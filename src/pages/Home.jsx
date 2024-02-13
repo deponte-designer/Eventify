2ed56737b6bda0c79c6b1df500c09b837a718173
Ponte, Nadia <deponte.work@gmail.com>
2024-02-13 08:16:44 +0000

    clean unsused carousel image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,45 +18,30 @@ function Home() {
           <Image
             className="d-block w-100 carousel-img"
             src={Slider1}
-            // src="https://placehold.co/600x200/000000/FFFFFF/png"
             alt="First slide"
           />
-          {/* <ExampleCarouselImage text="First slide" src="https://placehold.co/600x400/000000/FFFFFF/png" /> */}
           <Carousel.Caption className="carousel-caption-custom">
             <h2>Millions of Artist's profiles</h2>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <Image
             className="d-block w-100 carousel-img"
             src={Slider2}
-            // src="https://placehold.co/600x200/000000/FFFFFF/png"
             alt="Second slide"
           />
-          {/* <ExampleCarouselImage text="Second slide" src="https://placehold.co/600x400/000000/FFFFFF/png" /> */}
-          <Carousel.Caption  className="carousel-caption-custom">
+          <Carousel.Caption className="carousel-caption-custom">
             <h2>Events for all</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        {/* <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://placehold.co/600x200/000000/FFFFFF/png"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item> */}
+
       </Carousel>
     </div>
   );
 }
-   
+
 
 export default Home;
