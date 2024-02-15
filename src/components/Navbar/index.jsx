e4a1c973a7842eaadb350212aad30a4a38794ea4
Ponte, Nadia <deponte.work@gmail.com>
2024-02-15 01:26:51 +0000

    uploaded the logo for the navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Navbar, Nav, Container, Form, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Form, Button, Image } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import NavLogo from '../../assets/images/eventify-logo-nav.svg';
 
 
 let CustomNavbar = ({ onSearchSubmit }) => {
@@ -22,43 +23,66 @@ let CustomNavbar = ({ onSearchSubmit }) => {
   return (
     <Navbar className="bg-body-tertiary-opacity nav-opacity mb-3" expand="lg" data-bs-theme="dark" fixed="top">
       <Container>
-        
-        <Navbar.Brand className="" href="#">Eventify</Navbar.Brand>
+
+        {/* <Navbar.Brand className="" href="#">Eventify</Navbar.Brand> */}
+
+        <Navbar.Brand className="" href="#">
+          <Image
+            className="d-inline-block align-top"
+            src={NavLogo}
+            alt="Eventify logo"
+            height="35px"
+          />
+        </Navbar.Brand>
+
+        {/* <Container>
+          <Navbar.Brand href="#home">
+            <Image
+              src={NavLogo}
+              
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        </Container> */}
+
+
         <Form onSubmit={handleSearchSubmit} className="d-flex d-lg-none">
-              <Form.Control
-                type="search"
-                placeholder="Search by Artist..."
-                className="me-2"
-                aria-label="Search"
-                value={searchQuery}
-                // onChange={handleSearchChange}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-              <Button variant="outline-success" className="btn-outline-purple" type="submit">Search</Button>
-            </Form>
+          <Form.Control
+            type="search"
+            placeholder="Search by Artist..."
+            className="me-2"
+            aria-label="Search"
+            value={searchQuery}
+            // onChange={handleSearchChange}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <Button variant="outline-success" className="btn-outline-purple" type="submit">Search</Button>
+        </Form>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavLink className="nav-link" to="/" end>Home</NavLink>
             <NavLink className="nav-link" to="/artists" end>Artists</NavLink>
             <NavLink className="nav-link" to="/contact" end>Contact</NavLink>
-            
+
           </Nav>
         </Navbar.Collapse>
         <Form onSubmit={handleSearchSubmit} className="d-lg-flex d-none">
-              <Form.Control
-                type="search"
-                placeholder="Search by Artist..."
-                className="me-2"
-                aria-label="Search"
-                value={searchQuery}
-                // onChange={handleSearchChange}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-              <Button variant="outline-success" className="btn-outline-purple" type="submit">Search</Button>
-            </Form>
+          <Form.Control
+            type="search"
+            placeholder="Search by Artist..."
+            className="me-2"
+            aria-label="Search"
+            value={searchQuery}
+            // onChange={handleSearchChange}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <Button variant="outline-success" className="btn-outline-purple" type="submit">Search</Button>
+        </Form>
 
-            </Container>
+      </Container>
     </Navbar>
 
   );
